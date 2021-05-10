582cc967f898737197a2ceb4aa026268540dffaf
Usama Khalid <usamakhalidxo0@gmail.com>
2021-05-10 15:20:11 +0500

    retrying

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 router.use('*', function(req,res,next){
-    res.status(404).json({
+    res.status(404).send({
         message: 'not yet implemented!'
     })
 })
