b3e8ccf57ded3660edb5e70bcf743fee2f1f4f85
Usama Khalid <usamakhalidxo0@gmail.com>
2021-07-06 21:39:24 +0500

    re: fixing controller wrapper

--- helpers/controlerWrapper.js
+++ helpers/controlerWrapper.js
@@ -1,5 +1,5 @@
 module.exports= function(fn){
-    return function(req,res,next){
+    return async function(req,res,next){
         try {await fn(req,res,next);}
         catch(err){next(err)}
     }
